8089f45f93f5610452c3a18c5543be0502529132
BOYZA408 <BOYZA408@hotmail.com>
2017-07-07 04:03:06 +0700

    Add files via upload

---
+++ js/circlebar.js
@@ -0,0 +1,123 @@
+var bar = new ProgressBar.Circle(bar1, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#1d9d9f', width: 4 },
+    // Set default step function for all animate calls
+    step: function (state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value);
+        }
+
+    }
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '2rem';
+
+bar.animate(0.5);  // Number from 0.0 to 1.0
+
+var barr = new ProgressBar.Circle(bar2, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#1d9d9f', width: 4 },
+    // Set default step function for all animate calls
+    step: function (state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value);
+        }
+
+    }
+});
+barr.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+barr.text.style.fontSize = '2rem';
+
+barr.animate(0.6);  // Number from 0.0 to 1.0
+
+var bar3s = new ProgressBar.Circle(bar3, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#1d9d9f', width: 4 },
+    // Set default step function for all animate calls
+    step: function (state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value);
+        }
+
+    }
+});
+bar3s.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar3s.text.style.fontSize = '2rem';
+
+bar3s.animate(0.1);  // Number from 0.0 to 1.0
+
+var bar4s = new ProgressBar.Circle(bar4, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#1d9d9f', width: 4 },
+    // Set default step function for all animate calls
+    step: function (state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value);
+        }
+
+    }
+});
+bar4s.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar4s.text.style.fontSize = '2rem';
+
+bar4s.animate(0.1);  // Number from 0.0 to 1.0
